83ef6610ee611f827db7d46d08b9a029333b404c
WisdmLabs <WisdmLabs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-27 07:39:12 +0000

    removed reset code from render-recaptcha.js file
    git-svn-id: https://plugins.svn.wordpress.org/google-no-captcha-recaptcha/trunk@1207256 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- public/assets/js/render-recaptcha.js
+++ public/assets/js/render-recaptcha.js
@@ -15,24 +15,4 @@ jQuery( window ).load( function () {
         } );
     }
 
-    //jQuery("[type='recaptcha']").css({"transform":"scale(0.88)","transform-origin":"0","-webkit-transform":"scale(0.88)"});
-
-    jQuery( '.wpcf7-submit' ).click( function ( event ) {
-        var form = jQuery( this ).parents( 'form:first' );
-        jQuery( document ).ajaxComplete( function ( event, xhr, options ) {
-            jQuery("div [type='recaptcha']").find("iframe").css("opacity","1");
-            var response = xhr.responseText;
-            var occurance = response.match( /invalids/g );
-            if ( occurance === null )
-            {
-                var id = jQuery( "div[id^='wdm-nocapt-recapt-id-recaptcha-']", form ).attr( "id" );
-                
-                for ( var x = 0; x < a.length; x++ ) {
-                    if ( a[x] === id ) {
-                        grecaptcha.reset( x );
-                    }
-                }
-            }
-        } );
-    } );
 } );
