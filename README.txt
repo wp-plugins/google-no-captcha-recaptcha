ab86359a16825420d0b69c865760e6baf2d2deff
WisdmLabs <WisdmLabs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-27 06:50:30 +0000

    added version 3.0.3
    git-svn-id: https://plugins.svn.wordpress.org/google-no-captcha-recaptcha/tags/3.0.3@1207220 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,143 @@
+=== Google No CAPTCHA reCAPTCHA by WisdmLabs ===
+Contributors: WisdmLabs
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=info%40wisdmlabs%2ecom&lc=US&item_name=WisdmLabs%20Plugin%20Donation&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest
+Tags: nocaptcha recaptcha, contact form 7, Google nocaptcha recaptcha, contact form 7 security, spam filter, antispam, spam blocker, captcha, form security, security, wordpress captcha
+Requires at least: 3.9
+Tested up to: 4.2.2
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+The plugin adds a No CAPTCHA reCAPTCHA tag in Contact Form 7, which can be used to include a No CAPTCHA reCAPTCHA anti-spam field.
+
+== Description ==
+
+Research has shown that the reCAPTCHA field can be decoded by smart bots 99% of the time. Google has thus introduced a 'No CAPTCHA reCAPTCHA' API which has a risk analysis engine backing the verification process.
+Integrate this API into your Contact Form 7 forms, using this plugin.
+
+* The Google No CAPTCHA reCAPTCHA plugin provides an option to set a custom error message when the CAPTCHA field is not set or if a robot is detected.
+
+* The plugin is localization ready, which means you can display the error message in your local language.
+
+* It also provides support to display the CAPTCHA in multiple languages.
+
+* The plugin is multiform compatible.
+
+* The plugin is multisite compatible. Do ensure that you add the keys for every sub-domain in the plugin's settings.
+
+== Installation ==
+
+= Using The WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Search for 'google-nocaptcha-recaptcha'
+3. Click 'Install Now'
+4. Activate the plugin on the Plugin dashboard
+
+= Uploading in WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Navigate to the 'Upload' area
+3. Select `google-nocaptcha-recaptcha.zip` from your computer
+4. Click 'Install Now'
+5. Activate the plugin in the Plugin dashboard
+
+= Using FTP =
+
+1. Download `google-nocaptcha-recaptcha.zip`
+2. Extract the `google-nocaptcha-recaptcha` directory to your computer
+3. Upload the `google-nocaptcha-recaptcha` directory to the `/wp-content/plugins/` directory
+4. Activate the plugin in the Plugin dashboard
+
+= Setting up No Captcha reCaptcha =
+
+1. Create domain specific No Captcha reCaptcha key from https://www.google.com/recaptcha/admin#list
+2. From your site dashboard, go to Settings > Google No Captcha reCaptcha
+3. Enter your site key and secret key and save changes
+
+
+
+== Frequently Asked Questions ==
+
+= Where can I find the plugin's settings? =
+
+The plugin's settings can be found under 'Settings -> Google NoCaptcha ReCaptcha Settings'
+
+= Help! I've added the tag, but it doesn't seem to work! =
+
+In case you do not see a No CAPTCHA reCAPTCHA field, kindly ensure that you have added the needed API keys in the plugin settings.
+
+= How do I acquire a Secret Key and Site Key =
+
+In the plugin settings, you'll have to enter a Secret Key and a Site Key. You can get the keys, by logging into a Gmail account and registering your site, using this link: https://www.google.com/recaptcha/admin#list
+
+= How to set the captcha language? =
+
+Captcha language can be set from 'Settings -> Google NoCaptcha ReCaptcha Settings -> Language'
+
+= How to set custom error message? =
+
+Custom error messages can be set from 'Settings -> Google NoCaptcha ReCaptcha Settings -> Custom Error Messages'
+
+= How to Localize the messages displayed? =
+
+The error messages can be translated using the "goole-nocaptcha-recaptcha-locale.pot" file, present in the languages folder of the plugin.
+For translating the message in this file follow the below steps:
+
+1) Convert the .pot file to .mo file and add it to the languages folder of the plugin along with the translated .pot file.
+
+"goole-nocaptcha-recaptcha-locale-<language-code>.mo" and "goole-nocaptcha-recaptcha-locale-<language-code>.pot".
+
+For example, for German, name the files goole-nocaptcha-recaptcha-locale-de_DE.mo and goole-nocaptcha-recaptcha-locale-de_DE.pot
+
+2) Place the .po and .mo file in the languages folder with in plugin folder.
+
+== Screenshots ==
+
+1. No CAPTCHA reCAPTCHA settings page
+2. No CAPTCHA reCAPTCHA tag generator in in Contact Form 7
+3. No CAPTCHA reCAPTCHA tag in Contact Form 7 backend
+4. No CAPTCHA reCAPTCHA field on a Contact Form(Light theme)
+5. No CAPTCHA reCAPTCHA field on a Contact Form(Dark theme)
+
+== Changelog ==
+
+= 3.0.3 =
+* Fixed Minor Bug
+
+= 3.0.2 =
+* Fixed Minor Bug
+
+= 3.0.1 =
+* Fixed Minor Bug
+
+= 3.0 =
+* Multi-form support
+* Resolved Localization issue
+* Multi-language support
+* Field for custom error message added in admin dashboard settings
+
+= 2.0 =
+* Fixed compatibility issue with CF7v4.2+
+* file_get_content replaced with wp_remote_post, thanks to @SAM for his contribution [https://wordpress.org/support/topic/validation-problems-8]
+* Added id field to the No Captcha reCaptcha pane 
+* Fixed Notice "$class variable is not defined in nocaptcha_shortcode_handler() in public/includes/contact-form-7/class-wdm-contact-form-7-public.php on line 173"  
+* Made changes to the render_recaptcha.js file for future developement 
+
+= 1.1.2 =
+* Extra quote in id="wdm-nocapt-recapt-id"" removed
+
+= 1.1.1 =
+* Fixed the issue `Site Breaking while using version 1.1.0`
+
+= 1.1.0 =
+* Fixed the issue `Making noCaptcha required with Contact form 7` for [Contact form 7 version 4.1]
+* Fixed the issue `i-am-a-robot` where recaptcha returned false every time the form is submited for [PHP 5.6+]
+
+= 1.0.0 =
+* Plugin released.
+
+
+
+
+
---
+++ google-recaptcha-nocaptcha.php
@@ -0,0 +1,189 @@
+<?php
+
+/**
+ * @package   Wdm_Google_Nocaptcha_Recaptcha
+ * @author    WisdmLabs <support@wisdmlabs.com>
+ * @license   GPL-2.0+
+ * @link      http://wisdmlabs.com
+ * @copyright 2014 WisdmLabs 
+ *
+ * Plugin Name:       Google No CAPTCHA reCAPTCHA by WisdmLabs
+ * Description:       The plugin adds a No CAPTCHA reCAPTCHA tag in Contact Form 7, which can be used to include a No CAPTCHA reCAPTCHA anti-spam field.
+ * Version:           3.0.3
+ * Author:            WisdmLabs
+ * Author URI:        http://wisdmlabs.com
+ * Text Domain:       google-nocaptcha-recaptcha-locale
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ */
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+
+$wdm_recaptcha_section = array(
+	'site_key'						 => array(
+		'name'		 => 'Site Key',
+		'type'		 => 'text',
+		'default'	 => '',
+		'tip'		 => '<br><div class="captcha_setting_text">' . sprintf( __( 'Please enter the Site key provided by Google while %1$s registering your site for reCaptcha service %2$s', "google-nocaptcha-recaptcha-locale" ), '<a href="https://www.google.com/recaptcha/admin#list" target="_blank">', '</a>' ) . '</div>'
+	),
+	'secret_key'					 => array(
+		'name'		 => 'Secret Key',
+		'type'		 => 'text',
+		'default'	 => '',
+		'tip'		 => '<br><div class="captcha_setting_text">' . __( 'Copy the Secret key provided by Google in this textbox', "google-nocaptcha-recaptcha-locale" ) . '</div>'
+	),
+	'language'						 => array(
+		'name'			 => 'Language',
+		'type'			 => 'select',
+		'option_values'	 => captcha_language_codes(),
+		'tip'			 => '<div class="captcha_setting_text">' . __( 'Select the language in which the captcha should be rendered', "google-nocaptcha-recaptcha-locale" ) . '</div>'
+	),
+	'wdm_custom_unchecked_error_msg' => array(
+		'name'		 => 'Custom Error Messages',
+		'type'		 => 'text',
+		'default'	 => '',
+		'tip'		 => '<br><div class="captcha_setting_text">' . __( 'Custom error message to be displayed when NoCaptcha reCaptcha is not checked', "google-nocaptcha-recaptcha-locale" ) . '</i>'
+	),
+	'wdm_custom_bot_error_msg'		 => array(
+		'name'		 => '',
+		'type'		 => 'text',
+		'default'	 => '',
+		'tip'		 => '<br><div class="captcha_setting_text">' . __( 'Custom error message to be displayed when bot is detected', "google-nocaptcha-recaptcha-locale" ) . '</div>'
+	),
+);
+
+function captcha_language_codes() {
+	return array(
+		'ar' => __( 'Arabic', 'google-nocaptcha-recaptcha-locale' ),
+		'bn' => __( 'Bengali', 'google-nocaptcha-recaptcha-locale' ),
+		'bg' => __( 'Bulgarian', 'google-nocaptcha-recaptcha-locale' ),
+		'ca' => __( 'Catalan', 'google-nocaptcha-recaptcha-locale' ),
+		'zh-CN'	 => __( 'Chinese (Simplified)', 'google-nocaptcha-recaptcha-locale' ),
+		'zh-TW'	 => __( 'Chinese (Traditional)', 'google-nocaptcha-recaptcha-locale' ),
+		'hr'	 => __( 'Croatian', 'google-nocaptcha-recaptcha-locale' ),
+		'cs'	 => __( 'Czech', 'google-nocaptcha-recaptcha-locale' ),
+		'da'	 => __( 'Danish', 'google-nocaptcha-recaptcha-locale' ),
+		'nl'	 => __( 'Dutch', 'google-nocaptcha-recaptcha-locale' ),
+		'en-GB'	 => __( 'English (UK)', 'google-nocaptcha-recaptcha-locale' ),
+		'en'	 => __( 'English (US)', 'google-nocaptcha-recaptcha-locale' ),
+		'et'	 => __( 'Estonian', 'google-nocaptcha-recaptcha-locale' ),
+		'fil'	 => __( 'Filipino', 'google-nocaptcha-recaptcha-locale' ),
+		'fi'	 => __( 'Finnish', 'google-nocaptcha-recaptcha-locale' ),
+		'fr'	 => __( 'French', 'google-nocaptcha-recaptcha-locale' ),
+		'fr-CA'	 => __( 'French (Canadian)', 'google-nocaptcha-recaptcha-locale' ),
+		'de'	 => __( 'German', 'google-nocaptcha-recaptcha-locale' ),
+		'gu'	 => __( 'Gujarati', 'google-nocaptcha-recaptcha-locale' ),
+		'de-AT'	 => __( 'German (Austria)', 'google-nocaptcha-recaptcha-locale' ),
+		'de-CH'	 => __( 'German (Switzerland)', 'google-nocaptcha-recaptcha-locale' ),
+		'el'	 => __( 'Greek', 'google-nocaptcha-recaptcha-locale' ),
+		'iw'	 => __( 'Hebrew', 'google-nocaptcha-recaptcha-locale' ),
+		'hi'	 => __( 'Hindi', 'google-nocaptcha-recaptcha-locale' ),
+		'hu' => __( 'Hungarain', 'google-nocaptcha-recaptcha-locale' ),
+		'id' => __( 'Indonesian', 'google-nocaptcha-recaptcha-locale' ),
+		'it' => __( 'Italian', 'google-nocaptcha-recaptcha-locale' ),
+		'ja' => __( 'Japanese', 'google-nocaptcha-recaptcha-locale' ),
+		'kn' => __( 'Kannada', 'google-nocaptcha-recaptcha-locale' ),
+		'ko' => __( 'Korean', 'google-nocaptcha-recaptcha-locale' ),
+		'lv' => __( 'Latvian', 'google-nocaptcha-recaptcha-locale' ),
+		'lt' => __( 'Lithuanian', 'google-nocaptcha-recaptcha-locale' ),
+		'ms' => __( 'Malay', 'google-nocaptcha-recaptcha-locale' ),
+		'ml' => __( 'Malayalam', 'google-nocaptcha-recaptcha-locale' ),
+		'mr' => __( 'Marathi', 'google-nocaptcha-recaptcha-locale' ),
+		'no' => __( 'Norwegian', 'google-nocaptcha-recaptcha-locale' ),
+		'fa'	 => __( 'Persian', 'google-nocaptcha-recaptcha-locale' ),
+		'pl'	 => __( 'Polish', 'google-nocaptcha-recaptcha-locale' ),
+		'pt'	 => __( 'Portuguese', 'google-nocaptcha-recaptcha-locale' ),
+		'pt-BR'	 => __( 'Portuguese (Brazil)', 'google-nocaptcha-recaptcha-locale' ),
+		'pt-PT'	 => __( 'Portuguese (Portugal)', 'google-nocaptcha-recaptcha-locale' ),
+		'ro'	 => __( 'Romanian', 'google-nocaptcha-recaptcha-locale' ),
+		'ru'	 => __( 'Russian', 'google-nocaptcha-recaptcha-locale' ),
+		'sr'	 => __( 'Serbian', 'google-nocaptcha-recaptcha-locale' ),
+		'sk'	 => __( 'Slovak', 'google-nocaptcha-recaptcha-locale' ),
+		'sl'	 => __( 'Slovenian', 'google-nocaptcha-recaptcha-locale' ),
+		'es'	 => __( 'Spanish', 'google-nocaptcha-recaptcha-locale' ),
+		'es-419' => __( 'Spanish (Latin America)', 'google-nocaptcha-recaptcha-locale' ),
+		'sv' => __( 'Swedish', 'google-nocaptcha-recaptcha-locale' ),
+		'ta' => __( 'Tamil', 'google-nocaptcha-recaptcha-locale' ),
+		'te' => __( 'Telugu', 'google-nocaptcha-recaptcha-locale' ),
+		'th' => __( 'Thai', 'google-nocaptcha-recaptcha-locale' ),
+		'tr' => __( 'Turkish', 'google-nocaptcha-recaptcha-locale' ),
+		'uk' => __( 'Ukrainian', 'google-nocaptcha-recaptcha-locale' ),
+		'ur' => __( 'Urdu', 'google-nocaptcha-recaptcha-locale' ),
+		'vi' => __( 'Vietnamese', 'google-nocaptcha-recaptcha-locale' )
+	);
+}
+
+$wdm_recaptcha_settings = array( 'general' => array(
+		'name'	 => '',
+		'fields' => $wdm_recaptcha_section,
+	)
+);
+
+if ( ! defined( 'WDM_RECAPTCHA_OPTION_NAME' ) ) {
+	define( 'WDM_RECAPTCHA_OPTION_NAME', 'wdm_recaptcha_settings' );
+}
+/* ----------------------------------------------------------------------------*
+ * Public-Facing Functionality
+ * ---------------------------------------------------------------------------- */
+
+require_once( plugin_dir_path( __FILE__ ) . 'includes/class-wdm-gnr-retrieve-options.php' );
+$wdm_recaptcha_settings_values = Wdm_Class_Gnr_Retrieve_Options::get_instance();
+require_once( plugin_dir_path( __FILE__ ) . 'public/class-wdm-google-nocaptcha-recaptcha.php' );
+
+
+add_action( 'plugins_loaded', array( 'Wdm_Google_Nocaptcha_Recaptcha', 'get_instance' ) );
+/* ----------------------------------------------------------------------------*
+ * Dashboard and Administrative Functionality
+ * ---------------------------------------------------------------------------- */
+
+if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+
+	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-google-recaptcha-nocaptcha-admin.php' );
+	add_action( 'plugins_loaded', array( 'Wdm_Google_Nocaptcha_Recaptcha_Admin', 'get_instance' ) );
+}
+
+add_action( 'admin_enqueue_scripts', 'enqueue_admin_scripts');
+
+function enqueue_admin_scripts(){
+	
+	wp_register_style( 'wdm_settings_page_css',plugins_url( 'public/assets/css/settings-page-style.css' , __FILE__ ));
+   wp_enqueue_style( 'wdm_settings_page_css' );
+}
+/*
+ * Enable NoCaptcha ReCaptcha for specific modules. By default enabled for all modules
+ * In this associative array, value is name of the folder name for respective module
+ * 
+ * @since    1.0.0
+ * 
+ */
+
+$wdm_gnr_enabled_modules = apply_filters( 'enabled_modules', array(
+	'Contact Form 7' => 'contact-form-7'
+)
+);
+
+//include files of all enabled modules. 
+foreach ( $wdm_gnr_enabled_modules as $single_module_name => $single_module_slug ) {
+	//include files required on admin side
+	if ( is_admin() ) {
+		if ( file_exists( plugin_dir_path( __FILE__ ) . 'admin/includes/' . $single_module_slug . '/class-wdm-' . $single_module_slug . '-admin.php' ) ) {
+			@include_once ( plugin_dir_path( __FILE__ ) . 'admin/includes/' . $single_module_slug . '/class-wdm-' . $single_module_slug . '-admin.php');
+		}
+	}
+
+	//include files required on admin side as well as on frontend
+	if ( file_exists( plugin_dir_path( __FILE__ ) . 'includes/' . $single_module_slug . '/class-wdm-' . $single_module_slug . '.php' ) ) {
+		@include_once ( plugin_dir_path( __FILE__ ) . 'includes/' . $single_module_slug . '/class-wdm-' . $single_module_slug . '.php');
+	}
+
+	//include files required on frontend
+	if ( ! is_admin() ) {
+		if ( file_exists( plugin_dir_path( __FILE__ ) . 'public/includes/' . $single_module_slug . '/class-wdm-' . $single_module_slug . '-public.php' ) ) {
+			@include_once ( plugin_dir_path( __FILE__ ) . 'public/includes/' . $single_module_slug . '/class-wdm-' . $single_module_slug . '-public.php');
+		}
+	}
+}
